14ca503b5aecc3e9649c31562e12e2799860d2b7
lettuce <lettuce.lu@lettucelu.com>
2021-04-29 00:26:37 +0800

    add mongoose

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
-
 const port = 3000
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
 
@@ -15,4 +23,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`app is running on http://localhost:${port}`)
+  // console.log(db)
 })
