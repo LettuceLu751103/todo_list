03f6a0e593244329385fd7016f9a2dfb3195995c
lettuce <lettuce.lu@lettucelu.com>
2021-05-15 22:26:57 +0800

    modify mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -7,7 +7,7 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true 
 
 const db = mongoose.connection
 db.on('error', () => {
-  console.log('mongodb error!')
+  console.log(`mongodb error!: ${MONGODB_URI}`)
 })
 db.once('open', () => {
   console.log('mongodb connected!')
