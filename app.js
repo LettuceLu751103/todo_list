d451296f4f6fb61f3edccacd924610ab258b73fe
lettuce <lettuce.lu@lettucelu.com>
2021-05-15 22:03:30 +0800

    feat: heroku init

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
@@ -21,7 +21,7 @@ require('./config/mongoose')
 app.use(routes)
 
 
-app.listen(port, () => {
-  console.log(`app is running on http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`app is running on http://localhost:${PORT}`)
   // console.log(db)
 })
