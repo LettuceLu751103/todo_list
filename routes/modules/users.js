60d88cadad50cc01d718451cd91299a62aa504dc
lettuce <lettuce.lu@lettucelu.com>
2021-05-24 11:43:08 +0800

    add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -8,5 +8,14 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+router.post('/login', (req, res) => {
+
+})
+
+router.get('/register', (req, res) => {
+  console.log('GET /register')
+  res.render('register')
+})
+
 
 module.exports = router
