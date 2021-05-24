7f3478fea53286b39720a59b308c301c5d62c330
lettuce <lettuce.lu@lettucelu.com>
2021-05-24 15:49:09 +0800

    add express-session

--- app.js
+++ app.js
@@ -6,13 +6,20 @@ const PORT = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
-
+const session = require('express-session')
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 // 配置網頁模板區域
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+// 設定 express-session
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(methodOverride('_method'))
 
