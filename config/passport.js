ca352cc5158a0531922e2895288e34ba132108e1
lettuce <lettuce.lu@lettucelu.com>
2021-05-28 17:57:35 +0800

    feat: add bcrypt to login route

--- config/passport.js
+++ config/passport.js
@@ -4,6 +4,8 @@ const LocalStrategy = require('passport-local').Strategy
 
 const User = require('../models/user.js')
 
+const bcrypt = require('bcryptjs')
+
 module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
@@ -14,22 +16,25 @@ module.exports = app => {
         if (!user) {
           return done(null, false, { message: 'That email is not registered!' })
         }
-        if (user.password !== password) {
-          return done(null, false, { message: 'Email or Password incorrect.' })
-        }
-        return done(null, user)
+        return bcrypt.compare(password, user.password).then(isMatch => {
+          if (!isMatch) {
+            return done(null, false, { message: 'Email or Password incorrect.' })
+          }
+          return done(null, user)
+        })
+          .catch(err => done(err, false))
       })
-      .catch(err => done(err, false))
-  }))
 
-  // 設定序列化與反序列化
-  passport.serializeUser((user, done) => {
-    done(null, user.id)
-  })
-  passport.deserializeUser((id, done) => {
-    User.findById(id)
-      .lean()
-      .then(user => done(null, user))
-      .catch(err => done(err, null))
-  })
+    // 設定序列化與反序列化
+    passport.serializeUser((user, done) => {
+      done(null, user.id)
+    })
+    passport.deserializeUser((id, done) => {
+      User.findById(id)
+        .lean()
+        .then(user => done(null, user))
+        .catch(err => done(err, null))
+    })
+
+  }))
 }
